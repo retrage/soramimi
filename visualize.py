8d818bcf207d92f25d82046985b7207dc4a86bdc
Akira Moroo <retrage01@gmail.com>
2019-10-27 17:28:03 +0900

    Use fit_words to generate word cloud
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- visualize.py
+++ visualize.py
@@ -8,13 +8,30 @@ import nagisa
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
+EVAL_WEIGHT=True
 FONT_PATH = '/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc'
+STOP_WORDS = ['ある', 'ない', '無い', 'する', 'いい', 'もう', 'どう', '中略', 'ちょっと', 'そう', 'し', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def normalize(words):
     words = words.replace('\n', ' ')
     words = words.replace('\t', ' ')
     words = words.replace('（☆）', ' ')
+
     return words.strip()
+
+def weight(evaluation):
+    if not EVAL_WEIGHT:
+        return 1.0
+    if 'ジャンパー' in evaluation:
+        return 2.5
+    elif 'シャツ' in evaluation:
+        return 2.0
+    elif 'ミミかき' in evaluation:
+        return 1.5
+    elif '手ぬぐい' in evaluation:
+        return 1.0
+    else:
+        return 1.0
 
 def main():
     if len(sys.argv) < 2:
@@ -23,16 +40,23 @@ def main():
     with open(sys.argv[1], 'r') as fp:
         data = json.load(fp)
 
-    words_list = []
+    frequencies = {}
     for work in data:
         text = normalize(work['soramimi'])
         words = nagisa.extract(text, \
                 extract_postags=['名詞', '動詞', '形容詞' ,'副詞'])
-        words_list += words.words
+        evaluation = normalize(work['evaluation'])
+        word_weight = weight(evaluation)
+        for word in words.words:
+            if word in STOP_WORDS:
+                continue
+            if word in frequencies:
+                frequencies[word] += word_weight
+            else:
+                frequencies[word] = word_weight
 
-    stop_words = ['ある', 'ない', '無い', 'する', 'いい', 'もう', 'どう', '中略']
-    texts = ' '.join(words_list)
-    wordcloud = WordCloud(font_path=FONT_PATH, stopwords=set(stop_words), width=900, height=500).generate(texts)
+    wordcloud = WordCloud(font_path=FONT_PATH, \
+            width=900, height=500).fit_words(frequencies)
 
     plt.figure(figsize=(15, 12))
     plt.imshow(wordcloud)
